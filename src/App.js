527b962ee6c991814ad48dcdcaa9a3fe6ba52be0
ankitasingh01 <er.ankita9@gmail.com>
2022-03-15 12:50:55 +0530

    video file created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    //BEM NAMING CONVENTION
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let hit it guys lets build the super app</h1>
+      
+      {/* app container */}
+        {/* videos */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+        
     </div>
   );
 }
---
+++ src/Video.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Video.css';
+
+function Video() {
+  return (
+    <div className='video'>
+        <video src=''>
+
+        </video>
+    </div>
+  )
+}
+
+export default Video
